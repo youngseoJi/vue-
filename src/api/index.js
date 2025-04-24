bc53e2f47f1c7efabf701a8f7a3667198b224945
지영서 <estell.ys.j@gmail.com>
2025-04-24 17:50:41 +0900

    API 설정  공통화 중복나는 설정을 담는 방법  알아보기

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
 // axios Api
+// axios HTTP 요청 함수는 무조건 Promise 객체를 반환합니다.
+// return 필수 : 호출 결과를 반환해야 이후에 then/catch로 연결 가능하다.
+
+// url 공통화 작업 설정 : baseURL 설정
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+});
 
 // 회원가입
 function registerUser(userData) {
-  const url = 'http://localhost:3000/signup';
-
-  // axios HTTP 요청 함수는 무조건 Promise 객체를 반환합니다.
-  // return 필수 : 호출 결과를 반환해야 이후에 then/catch로 연결 가능하다.
-
-  return axios.post(url, userData);
+  // const url = 'http://localhost:3000/signup';
+  // return axios.post(url, userData);
+  instance.post('/signup', userData);
 }
 export { registerUser };
