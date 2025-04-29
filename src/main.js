5b385a614ab0fd81cf5dc17628a72b771254bcec
지영서 <estell.ys.j@gmail.com>
2025-04-29 17:20:45 +0900

    vuex 설치와 셋팅

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes/index';
+import store from '@/store/index'; // Vuex 스토어
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true; // Vue Devtools 작동을 위한 설정
 window.Vue = Vue; // 브라우저 콘솔에서 Vue 인식용
 
 new Vue({
-  router,
   render: h => h(App),
+  router, // Vue Router 사용
+  store, // Vuex 스토어 사용
 }).$mount('#app');
