7f04960f25bdbd47b8c383bdc521f67c06d7b6ae
지영서 <estell.ys.j@gmail.com>
2025-05-08 10:27:37 +0900

    component 구분하기, 로딩상태추가하기

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,26 +2,49 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          {{ postItem.title }}
-        </li>
+      <div v-if="isLoading">
+        Loading..
+      </div>
+      <ul v-else>
+        <postListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        />
+        <!-- <li v-for="postItem in postItems" :key="postItem._id">
+          <div class="post-title">
+            {{ postItem.title }}
+          </div>
+          <div class="post-contents">
+            {{ postItem.contents }}
+          </div>
+          <div class="post-time">
+            {{ postItem.createdAt }}
+          </div>
+        </li> -->
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import postListItem from '@/components/posts/postListItem.vue';
 import { fetchNoteDatas } from '@/api/index';
 export default {
+  components: {
+    postListItem,
+  },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchNoteDatas() {
+      this.isLoading = true;
       const { data } = await fetchNoteDatas();
+      this.isLoading = false;
       this.postItems = data.posts;
     },
   },
