8319298ac837b0347e0ed8c7c0bcdb2042e954dc
지영서 <estell.ys.j@gmail.com>
2025-05-08 17:46:23 +0900

    PostAddPage 학습 노트 등록 폼의 데이터 바인딩 및 이벤트 연결
    v-model 을 이용하여 데이터 연결
    submit 작동시 form 작동하도록 메소드 정하고 연결하기
    @submit=매소드명

--- src/components/posts/PostAddForm.vue
+++ src/components/posts/PostAddForm.vue
@@ -3,23 +3,36 @@
   <div class="contents">
     <h1 class="page-header">Create Post</h1>
     <div class="form-wrapper">
-      <form class="form" action="">
+      <!-- Form -->
+      <form class="form" @submit.prevent="submitForm">
         <div>
           <label for="title">Title: </label>
-          <input id="title" type="text" />
+          <input id="title" type="text" v-model="title" />
         </div>
         <div>
           <label for="contents">Contents: </label>
-          <textarea id="contents" type="text" rows="5" />
+          <textarea id="contents" type="text" rows="5" v-model="contents" />
         </div>
-        <button type="submt" class="btn">Create</button>
+        <button type="submit" class="btn">Create</button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('제출');
+    },
+  },
+};
 </script>
 
 <style scope>
